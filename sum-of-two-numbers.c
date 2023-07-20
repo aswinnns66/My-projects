c508f748bb21372ab5bcdfad92e3d1d5cac54c1a
Aswin Raj <rajaswin179@gmail.com>
2023-07-20 16:50:23 +0530

    initial commit

---
+++ sum-of-two-numbers.c
@@ -0,0 +1,13 @@
+    #include <stdio.h>
+    #include <stdlib.h>
+    
+    int main(void)
+     {
+        int number1,number2,sum;
+        printf("Enter two numbers: ");
+        scanf("%d%d",&number1,&number2);
+        sum=number1+number2;
+        printf("Result is : %d",sum);
+        
+        return 0;
+    }
